9970e3879bd66ff37ff40f42824045f762c453e6
David Gualvez <david-gualvez@outlook.ph>
2019-03-09 19:27:26 +0800

    ...

--- app/src/main/java/com/example/kotlinapp1/Home.kt
+++ app/src/main/java/com/example/kotlinapp1/Home.kt
@@ -2,6 +2,7 @@ package com.example.kotlinapp1
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
 import com.example.kotlinapp1.R.layout.activity_home
 import kotlinx.android.synthetic.main.activity_home.*
